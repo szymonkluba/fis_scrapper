d47abb54d0033a34b8f585fce8d22a3ad22f71fb
Szymon Kluba <szymonkluba@gmail.com>
2020-12-15 23:46:21 +0100

    Class for jumps of participant.

---
+++ jump.py
@@ -0,0 +1,9 @@
+class Jump:
+    distance = 0.0
+    points = 0.0
+
+    def __init__(self, distance: float):
+        self.distance = distance
+
+    def set_points(self, points: float):
+        self.points = points
