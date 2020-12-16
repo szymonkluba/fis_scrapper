c08ccbec4f3e6f4708522c26f58bd92f5075787b
Szymon Kluba <szymonkluba@gmail.com>
2020-12-16 11:20:19 +0100

    Table for simple display of scraped data

--- fis_scrapper.py
+++ fis_scrapper.py
@@ -5,30 +5,6 @@ from jump import Jump
 from participant import Participant
 from race import Race
 
-ABR_TOURNAMENTS = {
-    "Olympic Winter Games": "OWG",
-    "FIS Ski-Flying World Championships": "SFWC",
-    "World Ski Championships": "WSC",
-    "World Cup": "WC",
-    "FIS Junior World Ski Championships": "WJC",
-    "Viessmann FIS Ski Jumping World Cup": 'WC',
-    'Viessmann FIS Ski Jumping Qualification': 'QUA',
-    "Four Hill": "4H",
-    "Continental Cup": "COC",
-    "European Youth Olympic Festival": "EYOF",
-    "Grand Prix": "GP",
-    "Nordic Tournament": "NT",
-    "Universiade": "UVS",
-    "Youth Olympic Winter Games": "YOG",
-    "FIS Cup": "FC",
-    "FIS": "FIS",
-    "Junior": "JUN",
-    "National Championships": "NC",
-    "Asian Winter Games": "AWG",
-    "Alpen Cup": "OPA",
-    "Children": "CHI",
-    "Qualification": "QUA",
-}
 
 LINE_SCHEME = {
     'Rank': '',
